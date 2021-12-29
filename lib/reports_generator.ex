575bc57963f9a4211e10287869e99411dd3079e2
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-12-29 19:41:02 -0300

    Add function report_acc

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -2,7 +2,11 @@ defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
     |> File.stream!()
-    |> Enum.map(fn line -> parse_line(line) end)
+    |> Enum.reduce(report_acc(), fn line, report ->
+      [id, food_name, price] = parse_line(line)
+      [_food_name, price_now] = report[id]
+      Map.put(report, id, [food_name, price_now + price])
+    end)
   end
 
   defp parse_line(line) do
@@ -11,4 +15,6 @@ defmodule ReportsGenerator do
     |> String.split(",")
     |> List.update_at(2, &String.to_integer/1)
   end
+
+  defp report_acc(), do: Enum.into(1..30, %{}, &{Integer.to_string(&1), ["", 0]})
 end
